3c434dfe5948088cdf4c7fa50f53116f1c45882b
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2026-08-19 00:43:01 +0600

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      helo d dfddf dfd himel - Auto Reload Working! dfd
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur ipsa beatae adipisci iste. Cupiditate similique nostrum sequi deserunt, aspernatur delectus suscipit sit fugiat. Adipisci perferendis, corrupti sunt veritatis voluptates dolore?
+      Hello word
     </div>
   );
 }
